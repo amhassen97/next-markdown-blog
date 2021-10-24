7effdcf8bab5cee84dd30f58b61e52036f49fa68
Abdullah Hassen <am.hassen977@gmail.com>
2021-10-24 02:42:49 +0200

    followed tutorial

---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Head from 'next/head'
+import Post from '../components/Post'
+import { sortByDate } from '../utils'
+
+
+export default function Home({ posts }) {
+
+
+  return (
+    <div>
+      <Head>
+        <title>Dev Blog</title>
+      </Head>
+
+    <div className='posts'>
+      {posts.map((post, index)=>(
+        <Post post={post} key={index} />
+      ))}
+
+    </div>
+    </div>
+  )
+}
+
+
+export async function getStaticProps(context) {
+  // Get files from the posts dir
+  const files = fs.readdirSync(path.join('posts'))
+
+  // Get slug and frontmatter from posts
+
+  let posts = files.map(filename =>{
+    // Get slug
+    const slug = filename.replace('.md', '');
+
+    // Get frontmatter
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+    
+    const {data: frontmatter} = matter(markdownWithMeta)
+    
+    return{
+      slug,
+      frontmatter,
+
+    }
+  })
+
+  posts = posts.sort(sortByDate)
+
+  return {
+    props: {
+      posts,
+    }
+  };
+}
